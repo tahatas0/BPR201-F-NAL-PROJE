7ef0ec16a78ce4598beb533038530c5fbc3a0b3e
tahatas0 <wjqn9qcttp@privaterelay.appleid.com>
2025-11-02 21:54:01 +0300

    Refactor database connection error handling

--- includes/db.php
+++ includes/db.php
@@ -1,19 +1,16 @@
 <?php
-// WAMP'ınızın ayarları:
 $host = 'localhost';
-$dbname = 'ayakkabi_db'; // phpMyAdmin'de verdiğiniz veritabanı adı
+$dbname = 'ayakkabi_db';
 $user = 'root'; 
 $password = 'Tahatas06.'; 
     
 try {
-    // PDO kullanarak güvenli bağlantı (SQL Injection'a karşı önemlidir)
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Oturum başlangıcı
     session_start(); 
 
 } catch (PDOException $e) {
     echo "Veritabanı Bağlantı Hatası: " . $e->getMessage();
-    exit(); // Bağlantı hatası varsa sitenin devam etmesini engeller
+    exit(); 
 }
 ?>
